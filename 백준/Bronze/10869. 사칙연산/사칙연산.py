8a98585e5d43f83d9ab3441c21b81e65539449be
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-09 10:28:45 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,7 @@
+num1, num2 = input().split()
+
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+print(int(num1) * int(num2))
+print(int(int(num1) / int(num2)))
+print(int(num1) % int(num2))
