7ce38749fb209ff617e404100a305b553d4da1c7
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-10 10:29:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,15 +1,15 @@
 d1, d2, d3 = map(int, input().split())
 
-if d1 == d2 & d2 == d3 :
+if d1 == d2 and d2 == d3 :
     print(10000 + (d1 * 1000))
 elif d1 == d2 or d1 == d3 :
     print(1000 + (d1*100))
 elif d2 == d3 :
     print(1000 + (d2 * 100))
 else:
-    if d1 >= d2 and d1 >= d3:
-        print(d1 * 100)
-    elif d2 >= d1 and d2 >= d3:
-        print(d2 * 100)
+    if d2 <= d1 and d3 <= d1:
+        print(d1*100)
+    elif d1<= d2 and d3 <= d2:
+        print(d2*100)
     else:
-        print(d3 * 100)
+        print(d3*100)
