822d7b34b987fbeda258e0060cb4528931d10b65
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-25 02:04:36 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+function solution(arr, delete_list) {
+    var answer = [];
+    arr.forEach((num)=> {
+        if(!delete_list.includes(num)){
+            answer.push(num);
+        }
+    })
+    return answer;
+}
