59568476971010df3c15ef94fd54271f15c92f7a
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-26 15:34:29 +0900

    [level 0] Title: 그림 확대, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -0,0 +1,20 @@
+function solution(picture, k) {
+    var answer = [];
+    let arr1 = [];
+    picture.forEach((pic)=> {
+        arr1.push([...pic].map((dob)=> {
+            return dob = dob.repeat(k);
+        }))
+    })
+    arr1 = arr1.map((str)=> {
+        return str.join("");
+    })
+    arr1.forEach((str)=> {
+        for(let i = 0;i<k;i++){
+            answer.push(str);
+        }
+    })
+    return answer;
+}
+//row 배 
+//colum배 
