7337ce232aa52bdfbbac49607d132ee380371007
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-28 15:26:54 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,17 @@
+function solution(dot) {
+    var answer = 0;
+    if(dot[0] < 0){
+        if(dot[1] < 0){
+            answer = 3; 
+        }else{
+            answer = 2;
+        }
+    }else{
+        if(dot[1] <0){
+            answer = 4;
+        }else{
+            answer = 1;
+        }
+    }
+    return answer;
+}
