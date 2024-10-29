93abe7c6c1830b3ffffee70bb0271162a4862b09
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-29 16:52:20 +0900

    [level 0] Title: 암호 해독, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,14 +1,7 @@
 function solution(cipher, code) {
-    var answer = '';
-    let count = 1;
-    for(let i = 0;i<= cipher.length-1;i++){
-        if(count % code == 0){
-            answer +=cipher[i];
-            count++;
-        }else{
-            count++;
-        }
-        
-    }
-    return answer;
+  var answer = "";
+  for (let i = code - 1; i < cipher.length; i += code) {
+    answer += cipher[i];
+  }
+  return answer;
 }
