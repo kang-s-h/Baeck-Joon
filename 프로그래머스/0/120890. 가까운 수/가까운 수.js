87a5a12d43e0f6b8e2a8460a82fbd021b29cb0d8
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-10-30 18:19:10 +0900

    [level 0] Title: 가까운 수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,17 +1,9 @@
 function solution(array, n) {
-    var answer = 0;
-    const arr = array.map((str)=> {
-        return str = Math.abs(str - n);
-    })
-    const min = Math.min(...arr);
-    let a = arr.indexOf(min);
-    let b = arr.lastIndexOf(min);
-    if(a !== b){
-        answer = array[a] > array[b] ? array[b] : array[a];
-    }else{
-        answer = array[a];
-    }
+    const arr = array.map(str => Math.abs(str - n));
     
-        
-    return answer;
+    const min = Math.min(...arr);
+    
+    const closestValues = array.filter((_, i) => arr[i] === min);
+    
+    return Math.min(...closestValues);
 }
