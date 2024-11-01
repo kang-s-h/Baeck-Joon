0b8774e0ca48260443d071ad48c0f4036ec0be96
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-01 10:05:51 +0900

    [level 0] Title: 소인수분해, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,20 +1,14 @@
 function solution(n) {
        let answer = [];
     
-    while (n % 2 === 0) {
-        answer.push(2);
-        n /= 2;
-    }
-
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
-        while (n % i === 0) {
-            answer.push(i);
-            n /= i;
-        }
-    }
-
-    if (n > 2) {
-        answer.push(n);
+     let i = 2;
+    while (i <= n) {
+      if (n % i === 0) {
+        answer.push(i)     
+        n = n / i
+      } else {
+      i++        
+      }
     }
 
     return [...new Set(answer)];
