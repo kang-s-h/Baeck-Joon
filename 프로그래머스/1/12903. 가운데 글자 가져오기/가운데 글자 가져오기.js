181ab2f4128e87ab2ade682e7558b6bba11f7320
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-03 23:59:10 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,7 @@
 function solution(s) {
     var answer = '';
-    if(s.length % 2 !== 0){
-       answer = s[parseInt(s.length / 2)];
-    }else{
-        answer = s[(s.length / 2)-1]+s[s.length / 2];
-    }
+    let num = s.length / 2;
+    
+    answer = s.length % 2 !== 0 ? s[parseInt(num)] :s[num-1]+s[num];
     return answer;
 }
