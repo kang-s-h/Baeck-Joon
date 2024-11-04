3033781a0038f16f5f50d7549ce079765811b6dc
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-04 09:45:26 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = true;
+    if(s.length !== 4 && s.length !== 6){
+        answer = false;
+    }
+    for(let i = 0;i<s.length;i++){
+        if(isNaN(s[i])){
+            answer = false;
+            break;
+        }
+    }
+    
+    return answer;
+}
