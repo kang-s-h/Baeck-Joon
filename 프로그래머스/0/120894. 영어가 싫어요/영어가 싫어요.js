0de38dd7d99a79b438093f358eb0883be5a4057e
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-06 17:43:45 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,26 +1,10 @@
 function solution(numbers) {
     var answer = 0;
-    let str = numbers;
-    let number = {
-        zero : 0,
-        one : 1,
-        two : 2,
-        three : 3,
-        four : 4,
-        five : 5,
-        six : 6,
-        seven : 7,
-        eight : 8,
-        nine : 9,
-    
+    const number = ["zero","one","two","three", "four", "five", "six", "seven", "eight", "nine"]
+    for(let i = 0 ; i<number.length; i++){
+        numbers = numbers.split(number[i]).join(i)
     }
-    Object.keys(number).forEach((num,i,arr)=> {
-        while(str.includes(num)){
-            let loc = str.indexOf(num);
-           str = str.slice(0,loc)+number[num]+str.slice(loc+num.length);
-        }
-        
-    })
-    answer = +str;
+    answer = +numbers;
+    
     return answer;
 }
