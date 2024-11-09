98adaf0f58005a5d34a2035a5e94fe9cc6e51995
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-09 23:44:30 +0900

    [level 0] Title: 옹알이 (1), Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,19 +1,10 @@
 function solution(babbling) {
-    var answer = 0;
-    let arr = ["aya", "ye", "woo", "ma"]
-    let array = [...babbling];
-    for(let i = 0;i<4;i++){
-        let arra = array.map((str)=> {
-            return str.split(arr[i]).join(" ");
-        })
-        array = arra;
-        console.log(array);
-    }
-    let a = array.forEach((str)=> {
-        if(str.trim() == ""){
-            answer++;
-        }
-    })
-   
-    return answer;
+  var answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach(word => {
+    if (regex.test(word)) answer++;  
+  })
+
+  return answer;
 }
