87255faa998c21d16b32ab8e08b520d2e408d65b
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-23 17:03:33 +0900

    [level 0] Title: 안전지대, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,63 +1,33 @@
 function solution(board) {
-//     var answer = 0;
-//     let arr = [];
-//     let array = board.flat();
-//     array.forEach((num,index)=> {
-//         if(num == 1){
-//             arr.push(index);
-//         }
-//     })
-//     console.log(arr);
-//     let setArr = [1,-1];
-//     let len = board[0].length;
-//     setArr.push(len+1,len,len-1,-(len+1),-len,-(len-1));
+    var answer = 0;
     
-//     for(let i = 0;i<arr.length;i++){
-//         setArr.forEach((num)=> {
-//             if(array[arr[i]-num] !== undefined){
-//                     array[arr[i]-num] = 1;
-//             }
-//         })
-//     }
-//     array.forEach((num)=> {
-//         if(num == 0){
-//             answer++;
-//         }
-//     })
-//     return answer;
+    const dx = [-1,0,1,1,1,0,-1,-1];
+    const dy = [1,1,1,0,-1,-1,-1,0];
+    const len = board.length;
     
-    var dy = [-1, -1, -1, 0, 0, 1, 1, 1]
-    var dx = [-1, 0, 1, -1, 1, -1, 0, 1]
-    var len = board.length;
-    var solution = 0;
-    
-    for (var i = 0; i < len; i++) {
-        for (var j = 0; j < len; j++) {
-            if (board[i][j] == 1) {
-                board[i][j] = -1
+    for(let i= 0;i<len;i++){
+        for(let j=0;j<len;j++){
+            if(board[i][j] == 1){
+                board[i][j] = 2;
+            
+            
+            for(let k = 0;k<8;k++){
+                let x = i + dx[k];
+                let y = j + dy[k];
                 
-                for (var k = 0; k < 8; k++) {
-                    var y = i + dy[k];
-                    var x = j + dx[k];
-                    
-                    if ((0 <= y && y <= len - 1 ) && (0 <= x && x <= len - 1) && (board[y][x] != 1)) {
-                        board[y][x] = -1
-                    }
-                }
+                if((0 <= x && x < len ) &&(0 <= y && y < len ) && (board[x][y] != 1)){
+                    board[x][y] = 2;
+                }   
             }
         }
     }
-    
-    for (var i = 0; i < len; i++) {
-        for (var j = 0; j < len; j++) {
-            if (board[i][j] == -1) continue;
-            
-            solution++;
-        }
-    }
-    
-    return solution;
 }
-
-//한줄로 생각하고 t면 t+1 t-1 t+4,5,6 t-4,5,6
-//만약에 0이면 2로 
+    for(let i= 0;i<len;i++){
+        for(let j=0;j<len;j++){
+            if(board[i][j] == 0){
+                answer++;
+            }
+        }
+    }    
+    return answer;
+}
