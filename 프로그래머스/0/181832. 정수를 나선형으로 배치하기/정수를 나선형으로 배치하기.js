4a2234363112bcb06dac50b69c7a1ffa30294748
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-26 20:43:48 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,28 +1,30 @@
 function solution(n) {
-    const direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]; 
-     let setArr = [];
+    var answer = [];
+    const direction = [[0,1],[1,0],[0,-1],[-1,0]];
+    let row = 0,col = 0;
+    answer = Array.from({length : n},()=> Array(n).fill(0));
     
-    for(let i = 0;i<n;i++){
-        setArr.push(Array(n).fill(0));
-    }
+    let count = 0;
     
-    let row = 0, col = 0, count = 0;
-
-    for (let i = 1; i <= n ** 2; i++) {
-        setArr[row][col] = i; 
-        const nextRow = row + direction[count][0];
-        const nextCol = col + direction[count][1];
-
-        // 다음 위치가 경계를 벗어나거나 이미 채워진 경우, 방향 전환
-        if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || setArr[nextRow][nextCol] !== 0) {
-            count = (count + 1) % 4; // 방향 전환
+    for(let i=1;i<=n**2;i++){
+        answer[row][col] = i;
+        
+        let nextRow = row + direction[count][0];
+        let nextCol = col + direction[count][1];
+        
+        if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || answer[nextRow][nextCol] !== 0) {
+            count = (count + 1) % 4;
         }
-
-        // 새로운 방향으로 이동
         row += direction[count][0];
         col += direction[count][1];
     }
-
-    return setArr;
+    
+    
+    return answer;
 }
 
+    
+    //일단 현재 위치에 숫자 넣고 
+    //다음 이동했을 때 넘어가는지 확인 하기 
+    //너멍가면 바꾸기 
+    //반복 
