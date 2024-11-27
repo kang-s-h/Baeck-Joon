4e427c5e008d14eadda108b76b96b7a8c7b5b53b
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-27 21:38:29 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,20 +1,11 @@
 function solution(s) {
-    var answer = 0;
-    const numberWords = ["zero","one","two","three","four","five","six","seven","eight","nine"];
-    let string = s;
-    //console.log(a.indexOf('one')); 
-    //indexof() + str.length
-    
-    for(let i = 0;i<s.length;i++){
-        for(let j= 0;j<numberWords.length;j++){
-            if(string.includes(numberWords[j])){
-                let idx = string.indexOf(numberWords[j]);
-                string = string.slice(0,idx) +String(j)+string.slice(idx+numberWords[j].length);
-            }
-        }
-    }
-    answer = Number(string);
-    return answer;
-}
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
 
-//
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
