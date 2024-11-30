f303c11713d6ce58e64f38e2159b465895c8bb68
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-11-30 22:52:55 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,8 +1,34 @@
 function solution(n, arr1, arr2) {
-    return arr1.map((a, i) => {
-        // 이진수 변환 및 OR 연산으로 겹침 처리
-        const binary = (a | arr2[i]).toString(2).padStart(n, "0");
-        // 이진수를 #과 공백으로 변환
-        return binary.replace(/1/g, "#").replace(/0/g, " ");
-    });
+    var answer =  Array.from({ length: n }, () => Array(n).fill(0));
+    
+    const setArr = (arr) => {
+        arr.forEach((num,index)=> {
+            let change = num.toString(2);
+            
+            if(`${change}`.length < n){
+                let str="";
+                
+                for(let i = 0;i<n-change.length;i++){
+                    str +="0";
+                }
+                change = str + String(change);
+            }
+            change.split("").forEach((num,idx)=> {
+                answer[index][idx] += +num; 
+            })
+        })
+    }
+    setArr(arr1);
+    setArr(arr2);
+    
+    answer = answer.map((arr)=> {
+       return arr.map((num)=> {
+            if(num == 0){
+                return num = " ";
+            }else{
+                return num = "#";
+            }
+        }).join("");
+    })
+    return answer;
 }
