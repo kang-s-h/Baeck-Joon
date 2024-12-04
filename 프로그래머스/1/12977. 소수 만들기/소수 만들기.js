00cd0d71c9717fcef7bc03a84adf389cbeef6db6
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2024-12-04 20:01:36 +0900

    [level 1] Title: 소수 만들기, Time: 93.83 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,31 +1,26 @@
-function solution(nums) {
-    var answer = 0;
-    const [a,b,c] = nums.sort((a,b)=> b - a);
-    let arr = [];
-    
-    for(let i = 0;i<=a+b+c;i++){
-        arr.push(true);
-    }
-    arr[0] = arr[1] = false;
-    
-    for(let i = 2;i<a+b+c;i++){
-        if(arr[i]== true){
-            for (let j = i * i; j <= a+b+c; j += i) {
-                arr[j] = false;
-            }
-        }
-    }
-    console.log(arr);
-    for(let i = 0;i<nums.length-2;i++){
-        for(let k = i+1;k<nums.length-1;k++){
-            for(let j = k+1;j<nums.length;j++){
-                let add = nums[i] + nums[k] + nums[j];
-                if(arr[add] == true){
-                    answer++;
-                }
-            }
-        }
-    }
+function isPrime(num) {
 
-    return answer;
+  let flag = true;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+}
+
+function solution(nums) {
+  let answer = 0;
+  let num, size = nums.length;
+
+  for(let i = 0; i < size; i++) {
+    for (let j = i+1; j < size; j++) {
+      for (let k = j+1; k < size; k++) {
+        num = nums[i] + nums[j] + nums[k];
+        if(isPrime(num)) answer++;
+      }
+    }
+  }
+  return answer;
 }
