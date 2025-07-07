86263dd8b35da26745fb042cca7b2ce4958883f6
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2025-07-07 09:44:08 +0900

    [level 2] Title: 카펫, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -3,13 +3,10 @@ function solution(brown, yellow) {
     
     for(let i = 1;i<=Math.sqrt(yellow);i++){
         if(yellow % i === 0){
-            const a = (yellow / i) + 1;
-            const b = i + 1;
+            const a = (yellow / i) + 2;
+            const b = i + 2;
             
-            if((a + b)*2 === brown){
-                a >= b ? answer.push(a+1,b+1) : answer.push(b+1,a+1);
-                return answer;
-            }
+            if( a * b === brown + yellow) return [a,b];
         }
     }
     
