c255887d526838aa9bf36436510436598bec8efb
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2025-07-10 10:46:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,16 +1,14 @@
 function solution(n,a,b)
 {
-    var answer = 1;
+    var answer = 0;
     
-    let first = a>b ? b : a;
-    let second = a>b ? a : b;
+    let first = Math.min(a,b);
+    let second = Math.max(a,b);
     
-    if(first + 1 === second && first%2 !== 0 && second%2 === 0) return answer;
-    
-    while(first+1 !== second || (first%2 === 0 && second%2 !== 0)){
+    while(first !== second){
 
-        first = (first%2 === 0) ? first/2 : (first+1)/2;
-        second = (second%2 === 0) ? second/2 : (second+1)/2;
+        first = Math.ceil(first/2);
+        second = Math.ceil(second/2);
         
         answer++;
     }
