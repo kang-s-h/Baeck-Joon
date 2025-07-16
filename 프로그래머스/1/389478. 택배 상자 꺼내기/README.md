e3a00cb9150a35fff660713430936755ff3b9eeb
kang-s-h <156052023+kang-s-h@users.noreply.github.com>
2025-07-16 20:52:58 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.30 ms
+메모리: 33.4 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 16일 20:42:52
+2025년 07월 16일 20:52:56
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,46 +1,36 @@
 function solution(n, w, num) {
-    var answer = 0;
-    
-    let storage = [];
-    let arr = [];
-    let count = 0;
-    let idx = 0;
-    const remain = Math.floor((num - 1) / w);
-    
-    for(let i = 1; i <= n ; i++){
-        arr.push(i);
-        if(i % w === 0 || i === n){
-            
-            if(i===n && n % w !== 0){
-                for(let j = n%w; j<w; j++){
-                    arr.push(0);
+    const storage = [];
+    let currentRow = [];
+
+    for (let i = 1; i <= n; i++) {
+        currentRow.push(i);
+
+        if (i % w === 0 || i === n) {
+            const isReversedRow = storage.length % 2 !== 0;
+
+            if (i === n && n % w !== 0) {
+                const paddingCount = w - (n % w);
+                for (let j = 0; j < paddingCount; j++) {
+                    currentRow.push(0);
                 }
             }
-            
-            if(count % 2 !== 0){
-                arr.reverse();
+
+            if (isReversedRow) {
+                currentRow.reverse();
             }
-            
-            storage.push(arr);
-            
-            arr = [];
-            count++;
-        }
-       
-        
-    }
-    
-    storage[remain].forEach((item,index)=> {
-        if(item === num){
-            idx = index;
-        }
-    })
-    
-    for(let k = remain; k<storage.length;k++){
-        if(storage[k][idx] !== 0){
-            answer++;
+
+            storage.push(currentRow);
+            currentRow = [];
         }
     }
-    
+
+    const targetRowIndex = Math.floor((num - 1) / w);
+    const targetColIndex = storage[targetRowIndex].indexOf(num);
+
+    const answer = storage
+        .slice(targetRowIndex)
+        .filter(row => row[targetColIndex] !== 0)
+        .length;
+
     return answer;
 }
